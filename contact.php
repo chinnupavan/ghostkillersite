60d5b1ad87d5f24e97ed1a8e98dbc0c0bb1ec540
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-06-08 11:39:12 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,23 @@
-<?php 
-if(isset($_['btn_send'])){
-    echo "hi";
+<?php
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $toEmail = 'spamstuff920@gmail.com';
+    $emailSubject = 'New email from your contant form';
+    $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+
+    $bodyParagraphs = ["Name: {$name}","Phone : {$phone}", "Email: {$email}", "Message:", $message];
+    $body = join(PHP_EOL, $bodyParagraphs);
+
+    if (mail($toEmail, $emailSubject, $body, $headers)) {
+    header('Location: thank-you.html');
+        } 
+        else {
+    $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
 }
- 
+
 ?>
